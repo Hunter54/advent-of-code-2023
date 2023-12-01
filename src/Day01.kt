f74992fcddd74f644f46d5aa1eb7343ee7a11124
Valase Ionut <ionutvalase@gmai.com>
2023-12-01 21:50:09 +0200

    - day1 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,12 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val finalValue = input.fold(0) { sum, line ->
+            val firstDigit = line.first { it.isDigit() }
+            val lastDigit = line.last { it.isDigit() }
+            val lineValue = listOf(firstDigit,lastDigit).joinToString()
+            sum + lineValue.toInt()
+        }
+        return finalValue
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +15,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
