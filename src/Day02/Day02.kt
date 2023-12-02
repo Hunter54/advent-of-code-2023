9474d224b70f24f3e0b8596c678c460c05951374
Valase Ionut <ionutvalase@gmai.com>
2023-12-02 14:27:07 +0200

    - day2 part 2

--- src/Day02/Day02.kt
+++ src/Day02/Day02.kt
@@ -3,6 +3,7 @@ package Day02
 import readInput
 
 typealias Round = List<GrabbedCubes>
+
 data class Game(
     val gameNumber: Int,
     val rounds: List<Round>
@@ -12,27 +13,28 @@ data class GrabbedCubes(
     val color: String,
     val amount: Int
 )
+
 fun main() {
 
-    fun splitLine(line: String): Game{
+    fun parseLine(line: String): Game {
         val gameRegex = Regex("""(\d+): (.+)""")
         val roundRegex = Regex("""(\d+) (\w+)""")
 
         val matches = gameRegex.find(line) ?: error("No match")
         val gameId = matches.groupValues[1].toInt()
         val roundsSplit = matches.groupValues[2].trim().split(";")
-        val rounds = roundsSplit.map {round ->
+        val rounds = roundsSplit.map { round ->
             roundRegex.findAll(round).map {
-                GrabbedCubes(it.groupValues[2],it.groupValues[1].toInt())
+                GrabbedCubes(it.groupValues[2], it.groupValues[1].toInt())
             }.toList()
         }
-        return Game(gameId,rounds)
+        return Game(gameId, rounds)
     }
 
     fun part1(input: List<String>): Int {
         val numberOfCubes = mapOf("red" to 12, "green" to 13, "blue" to 14)
         val filteredGames = input.map {
-            splitLine(it)
+            parseLine(it)
         }.filter {
             !it.rounds.any { round ->
                 round.any { grabbedCubes ->
@@ -45,11 +47,28 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val games = input.map {
+            parseLine(it)
+        }
+        val powerOfCubes = games.sumOf { game ->
+            val mapMinimumValues = mutableMapOf("red" to 0, "green" to 0, "blue" to 0)
+            game.rounds.forEach { round ->
+                round.forEach { cubes ->
+                    if (cubes.amount > mapMinimumValues[cubes.color]!!) {
+                        mapMinimumValues[cubes.color] = cubes.amount
+                    }
+                }
+            }
+            val power = mapMinimumValues.values.fold(1) { power, value ->
+                power * value
+            }
+            power
+        }
+        return powerOfCubes
     }
 
     val testInput = readInput("Day02/Day02_test")
-    check(part1(testInput) == 8)
+    check(part2(testInput) == 2286)
 
     val input = readInput("Day02/Day02")
     println(part1(input))
