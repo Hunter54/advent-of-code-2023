cd1b884f7efe1cfb78ab891e60b9f9fbf0b97d56
Valase Ionut <ionutvalase@gmai.com>
2023-12-03 13:52:33 +0200

    - day3 part 1 and 2

---
+++ src/Day04/Day04.kt
@@ -0,0 +1,21 @@
+package Day04
+
+import readInput
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val testInput = readInput("Day04/Day04_test")
+    check(part1(testInput) == 0)
+
+    val input = readInput("Day04/Day04")
+    println(part1(input))
+    println(part2(input))
+}
